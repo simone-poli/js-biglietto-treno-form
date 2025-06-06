ee84e71d87075cf1cb97989b181ecffad22a934b
simone-poli <polismn97@gmail.com>
2025-06-06 18:40:57 +0200

    push name in ticket final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
-const formEl = document.querySelector('form');
+const selectName = document.getElementById("full-name");
+const selectKm = document.getElementById("kilometers");
+const selectAge = document.getElementById("age");
 
-formEl. addEventListener('submit', (event) => {
-    event. preventDefault();
+
+const fullNameEl = document.querySelector(".full-name")
+const priceEl = document.querySelector(".price");
+const codCp = document.querySelector(".cod")
+const carrozzaEl = document.querySelector(".carrozza");
+
+
+
+
+
+const formEl = document.querySelector("form");
+
+ 
+
+
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+const priceInitial = kilometers.value * 0.21;
+console.log(priceInitial);
+
+const fullNameValue = selectName.value;
+
+let priceValue;
+if (selectAge.value === "minorenne") { 
+    priceValue = priceInitial - (priceInitial * 20) / 100;
+} else if (selectAge.value === "over65") {
+    priceValue = priceInitial - (priceInitial * 40) / 100;
+} else {
+    priceValue = priceInitial;
+}
+priceEl.innerHTML = priceValue.toFixed(2);
+
+
+
+
+
+
+
+
+
+
+fullNameEl.innerHTML = fullNameValue;
+
+
 })
 
-console.log(formEl);
+
+
